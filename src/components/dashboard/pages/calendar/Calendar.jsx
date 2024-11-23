3134cc3b9feb3fa892b16460bd4fdf8054eb8dc7
Tommy Qu <tommyqu03@gmail.com>
2024-11-22 21:16:27 -0500

    remove hardcoded event

--- src/components/dashboard/pages/calendar/Calendar.jsx
+++ src/components/dashboard/pages/calendar/Calendar.jsx
@@ -34,13 +34,13 @@ const Calendar = () => {
   }, []);
 
   const events = [
-    {
-      id: 15,
-      title: "Lease Ending for Thomas Qu",
-      start: new Date(2024, 10, 29, 8, 0),
-      end: new Date(2024, 10, 29, 9, 0),
-      type: "primary",
-    },
+    // {
+    //   id: 15,
+    //   title: "Lease Ending for Thomas Qu",
+    //   start: new Date(2024, 10, 29, 8, 0),
+    //   end: new Date(2024, 10, 29, 9, 0),
+    //   type: "primary",
+    // },
   ];
 
   const getTodayDate = () => {
